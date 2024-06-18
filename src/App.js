ae020fe33cfa096f5fdf41b8a2d796fc170206ff
asnda <asandamabuza69@gmail.com>
2024-06-18 08:17:29 +0200

    final website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
 import MenuPage from './components/MenuPage';
 import Navigation from './components/Navigation';
-import './styles.css';
-
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <Navigation />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/about" component={AboutPage} />
-          <Route path="/menu" component={MenuPage} />
-        </Switch>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <Navigation />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/menu" element={<MenuPage />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
